6ab659365ae35c3ad63dde25d205652260d7b217
Alexander Zhukov <al.zhukoff@gmail.com>
2014-07-28 01:08:29 +0700

    Rename project

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 cd ~/
-mkdir .bash_task_runner
-cd .bash_task_runner
-curl -L https://raw.githubusercontent.com/nogizhopaboroda/bash_task_runner/master/runner.sh > runner.sh
+mkdir .taruntella
+cd .taruntella
+curl -L https://raw.githubusercontent.com/nogizhopaboroda/taruntella/master/runner.sh > runner.sh
 chmod 777 runner.sh
 shopt -s expand_aliases
-alias barun=~/.bash_task_runner/runner.sh
+alias taruntella=~/.taruntella/runner.sh
 alias
-echo 'alias barun=~/.bash_task_runner/runner.sh' >> ~/.zshrc
-echo 'Bash task runner successfuly installed'
-echo 'Reopen your terminal and type "barun"'
+echo 'alias taruntella=~/.taruntella/runner.sh' >> ~/.zshrc
+echo 'Taruntella runner successfuly installed'
+echo 'Reopen your terminal and type "taruntella"'
